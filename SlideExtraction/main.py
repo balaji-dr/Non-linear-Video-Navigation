998879cb501e9881a6089d4106afcf9e568dd259
Balaji D R <drbalaji97@gmail.com>
2019-01-05 19:29:55 +0530

    extracted timestamp of frame

--- SlideExtraction/main.py
+++ SlideExtraction/main.py
@@ -1,11 +1,15 @@
 import cv2
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 vidcap = cv2.VideoCapture('/Users/balajidr/Developer/FYP_TEMP/videos/trainvideo.mp4')
-success,image = vidcap.read()
+success, image = vidcap.read()
 count = 0
 while success:
-  if count % 30 == 0:
-    cv2.imwrite('/Users/balajidr/Developer/FYP_TEMP/SlideExtraction/slides/' + 'frame%d.jpg' % count, image)
-  # cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
+    if count % 30 == 0:
+        timestamp = vidcap.get(cv2.CAP_PROP_POS_MSEC)/1000.0
+        cv2.imwrite(BASE_DIR + '/SlideExtraction/slides/' + 'frame%d - %d.jpg' % (count, timestamp), image)
+        print('Extracted Frame %d of Time %d ' % (count, timestamp), success)
+    success, image = vidcap.read()
+    count += 1
